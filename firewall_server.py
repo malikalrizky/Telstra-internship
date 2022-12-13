43d77993296dee676863b72a46ba4ed53d7c109a
malikalrizky <110241221+malikalrizky@users.noreply.github.com>
2022-12-13 16:05:07 +0700

    commit

--- firewall_server.py
+++ firewall_server.py
@@ -6,17 +6,29 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 host = "localhost"
 port = 8000
 
-#########
-# Handle the response here 
 def block_request(self):
-    print("Blocking request")
+    self.send_error(403, "Request blocked due to firewall")
 
 def handle_request(self):
+    bad_headers = {
+        "suffix": "%>//",
+        "c1": "Runtime",
+        "c2": "<%",
+        "DNT": "1",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    if self.path == "/tomcatwar.jsp":
+        for bad_header_key in bad_header_keys:
+            for bad_header_key, bad_header_value in self.bad_headers.items():
+                if bad_header_key in self.headers and self.headers[bad_header_key] == bad_header_value:
+                    return block_request(self)
+
     self.send_response(200)
     self.send_header("content-type", "application/json")
     self.end_headers()
 
-#########
+    self.wfile.write({ "success": True })
 
 
 class ServerHandler(BaseHTTPRequestHandler):
